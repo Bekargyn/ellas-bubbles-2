792d20bd8a077a11c3f7b369ce383fbcacfab85f
Bek Abdrakhmanov <Bekargyn@gmail.com>
2020-05-13 14:10:42 -0500

    moved middle container to the .top

--- src/App.js
+++ src/App.js
@@ -130,83 +130,86 @@ function App() {
             </div>
           </div>
         </div>
-      </div>
-      <div className="middle">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <div className="img-col1">
-                <img
-                  className="img-fluid"
-                  src="../../images/col1.png"
-                  alt="bath"
-                />
+        <div className="middle">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div className="img-col1">
+                  <img
+                    className="img-fluid"
+                    src="../../images/col1.png"
+                    alt="bath"
+                  />
+                </div>
+                <div className="nameTag">
+                  ELLA INWARD SWING STAINLESS STEEL DOOR WALK IN TUB
+                </div>
+                <div className="description">
+                  <p>
+                    If your bathroom less then spacious, a stainless steel and
+                    tempered glass inward swing doorwalk-in bathtub can fit
+                    right in. The doors opens toward inside of the bathtub,
+                    which prevent you from knoking and bumping existing
+                    bathroomfixtures. Our unique design with tempured glassand
+                    stainless adds a sophisticated touch to any design.
+                  </p>
+                </div>
+                <div className="view-btn">
+                  <button>VIEW DETAILS</button>
+                </div>
               </div>
-              <div className="nameTag">
-                ELLA INWARD SWING STAINLESS STEEL DOOR WALK IN TUB
+              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div className="img-col1">
+                  <img
+                    className="img-fluid"
+                    src="../../images/col2.png"
+                    alt="bath"
+                  />
+                </div>
+                <div className="nameTag">
+                  ELLA OUTWARD SWING DOOR WALK IN TUB
+                </div>
+                <div className="description">
+                  <p>
+                    For bathers who require maximum accessibility, an outward
+                    swing door walk-in bathtun is the perfect choise. With
+                    option like wheelchair-accessible transfer bathtubs or a
+                    standart demension walk-in bathtub, we offer a variety of
+                    modelsto suit your space. these walk-in bathtubs can easily
+                    accomodate larger individuals or bathrooms.
+                  </p>
+                </div>
+                <div className="view-btn">
+                  <button>VIEW DETAILS</button>
+                </div>
               </div>
-              <div className="description">
-                <p>
-                  If your bathroom less then spacious, a stainless steel and
-                  tempered glass inward swing doorwalk-in bathtub can fit right
-                  in. The doors opens toward inside of the bathtub, which
-                  prevent you from knoking and bumping existing
-                  bathroomfixtures. Our unique design with tempured glassand
-                  stainless adds a sophisticated touch to any design.
-                </p>
-              </div>
-              <div className="view-btn">
-                <button>VIEW DETAILS</button>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <div className="img-col1">
-                <img
-                  className="img-fluid"
-                  src="../../images/col2.png"
-                  alt="bath"
-                />
-              </div>
-              <div className="nameTag">ELLA OUTWARD SWING DOOR WALK IN TUB</div>
-              <div className="description">
-                <p>
-                  For bathers who require maximum accessibility, an outward
-                  swing door walk-in bathtun is the perfect choise. With option
-                  like wheelchair-accessible transfer bathtubs or a standart
-                  demension walk-in bathtub, we offer a variety of modelsto suit
-                  your space. these walk-in bathtubs can easily accomodate
-                  larger individuals or bathrooms.
-                </p>
-              </div>
-              <div className="view-btn">
-                <button>VIEW DETAILS</button>
-              </div>
-            </div>
-            <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-              <div className="img-col1">
-                <img
-                  className="img-fluid"
-                  src="../../images/col3.png"
-                  alt="bath"
-                />
-              </div>
-              <div className="nameTag">ELLA TWO SEAT WALK IN TUBS</div>
-              <div className="description">
-                <p>
-                  Our two seat walk-ih bathtubs provide unique bathing
-                  experience for couples. With two outward-swing door bathtub
-                  sizes to choose from, you'll be able to share a massaging,
-                  spa-like bath with your loved one from the comfortof your own
-                  home.
-                </p>
-              </div>
-              <div className="view-btn">
-                <button>VIEW DETAILS</button>
+              <div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div className="img-col1">
+                  <img
+                    className="img-fluid"
+                    src="../../images/col3.png"
+                    alt="bath"
+                  />
+                </div>
+                <div className="nameTag">ELLA TWO SEAT WALK IN TUBS</div>
+                <div className="description">
+                  <p>
+                    Our two seat walk-ih bathtubs provide unique bathing
+                    experience for couples. With two outward-swing door bathtub
+                    sizes to choose from, you'll be able to share a massaging,
+                    spa-like bath with your loved one from the comfortof your
+                    own home.
+                  </p>
+                </div>
+                <div className="view-btn">
+                  <button>VIEW DETAILS</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
       <div className="bottom">
         <input
           onClick={goToTop}
